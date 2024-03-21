b8e53ed7192af8782411731ef6e94f14c83df1dc
ek <edgars@uso.lv>
2024-03-21 12:02:10 +0200

    Read fails nolasa datumu un vertibu

--- read.py
+++ read.py
@@ -1,1 +1,18 @@
+import json
+
 #Sis bus read  prieks first.py
+print("Start")
+filePath = "/home/ek/testa-repo/collectedDataJson.json"
+
+with open(filePath, "r") as file:
+    data = json.load(file)
+
+for specificData in data:
+    if specificData["Index"] == 0:
+        if specificData["Name"] == "LV":
+            continue
+        print(specificData["Name"] + " - " + specificData["Value"])
+
+
+#print(data)
+print("Done")
